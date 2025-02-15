98ab4bb9303ac026733e73862b51b0cf98bfd00b
Neelima137 <devadineelima137@gmail.com>
2025-02-15 18:13:02 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "name" {
+  type    = string
+  default = "test-1"
+}
+variable "location" {
+  type    = string
+  default = "us-central1"
+
+}
+variable "project" {
+  type    = string
+  default = "cts07-devadin"
+
+}
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+
+}
+
+
